e7e684484c9591c05ad19957346390aea75b5c9a
sumire-kcg <sumirezhengmu@gmail.com>
2026-04-07 16:15:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function linkPage1(){
+    window.location.href="https://www.bing.com/ck/a?!&&p=6cc987cc4c79ba01042c9eca9f5955361b13289f4526b8d95f587ef510d75c37JmltdHM9MTc0MDUyODAwMA&ptn=3&ver=2&hsh=4&fclid=3bcfabc8-d732-65e5-2b38-b845d61f6443&psq=%e6%85%b6%e6%87%89%e7%be%a9%e5%a1%be%e9%ab%98%e6%a0%a1%e6%bc%94%e5%8a%87%e9%83%a8&u=a1aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9rb2hzX2RyYW1hLw&ntb=1"
+}
+function linkPage2(){
+    window.location.href="https://www.bing.com/ck/a?!&&p=9059701fd9437cdde4ea1a8a6387cac0109825bfc55d62fc32774b75b1654c4aJmltdHM9MTc0MDUyODAwMA&ptn=3&ver=2&hsh=4&fclid=3bcfabc8-d732-65e5-2b38-b845d61f6443&psq=%e6%85%b6%e6%87%89%e7%be%a9%e5%a1%be%e9%ab%98%e6%a0%a1%e6%bc%94%e5%8a%87%e9%83%a8x&u=a1aHR0cHM6Ly90d2l0dGVyLmNvbS9rb2hzX2RyYW1h&ntb=1"
+}
